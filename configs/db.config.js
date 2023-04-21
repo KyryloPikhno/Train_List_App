a8e8059678fabffa44b08539d0245bbe5e6dcc00
KyryloPikhno <Mr.Good@i.ua>
2023-04-22 02:34:42 +0300

    added joi

--- configs/db.config.js
+++ configs/db.config.js
@@ -5,6 +5,6 @@ module.exports = {
     username: process.env.USERNAME || 'test',
     password: process.env.PASSWORD || 'test',
     database: process.env.DATABASE || 'test',
+    port: process.env.DB_PORT|| 3306,
     host: process.env.HOST || 'test',
-    port: process.env.DB_PORT|| 3306
 };
